2c9c3688a17413a60696176cb30a1c16686db25a
Thibaut Wojciak <thibaut2.wojciak@sfr.fr>
2021-01-01 17:37:29 +0100

    Correction d'un léger problème

--- src/controleur/ControleurMain.php
+++ src/controleur/ControleurMain.php
@@ -24,7 +24,7 @@ class ControleurMain
     }
 
     function getHomeAffichage(Request $rq, Response $rs, array $args): Response {
-        $vue = new VueParticipant([]);
+        $vue = new VueParticipant([], $this->c);
 
         $this->htmlvars['basepath'] = $rq->getUri()->getBasePath();
         $rs->getBody()->write($vue->render(1, $this->htmlvars));
